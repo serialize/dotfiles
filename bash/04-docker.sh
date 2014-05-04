e6130be8c84f71b77d87a9ae42683c99c1624c09
Frank Binder <github@serialize.eu>
2014-05-04 23:03:49 +0200

    merge

---
+++ bash/04-docker.sh
@@ -0,0 +1,74 @@
+#!/bin/bash 
+
+alias doc-mr="docker-container-most-recent"
+alias doc-dmr="docker-container-diff-most-recent"
+alias doc-i="docker-container-ip"
+alias doc-imr="docker-container-inspect-most-recent"
+alias doc-ra="docker-container-remove-all"
+alias doc-ranr="docker-container-remove-all-non-running"
+alias doi-mr="docker-image-most-recent"
+alias doi-ra="docker-image-remove-all"
+alias doi-ro="docker-image-remove-orphan"
+alias dor-i="docker run -i -t"
+alias dob="docker build -t"
+
+function docker-container-most-recent() {
+    docker ps | grep -v ^CONTAINER | head -n1 | awk '{print $1}'
+}
+
+function docker-container-diff-most-recent() {
+    LAST_CONTAINER=$(docker-container-most-recent)
+
+    if [ ! -z "$LAST_CONTAINER" ]
+    then
+        docker diff $LAST_CONTAINER
+    else
+        echo "There are no running containers!"
+    fi
+}
+
+function docker-container-ip() {
+    CONTAINER_ID=$1
+    if [ -z "$CONTAINER_ID" ]
+    then
+        CONTAINER_ID=$(docker-container-most-recent)
+    fi
+    if [ ! -z "$CONTAINER_ID" ]
+    then
+        docker inspect --format '{{ .NetworkSettings.IPAddress }}' $CONTAINER_ID
+    else
+        echo "There are no running containers!"
+    fi
+}
+
+function docker-container-inspect-most-recent() {
+    LAST_CONTAINER=$(docker-container-most-recent)
+    if [ ! -z "$LAST_CONTAINER" ]
+    then
+        docker inspect $LAST_CONTAINER
+    else
+        echo "There are no running containers!"
+    fi
+}
+
+function docker-container-remove-all() {
+    docker ps -a | grep -v ^CONTAINER | awk '{print $1}' | xargs -rI % sh -c "docker kill %; docker rm %"
+}
+
+function docker-container-remove-all-non-running() {
+    docker ps -a | grep -v ^CONTAINER | grep Exit | awk '{print $1}' | xargs -rI % sh -c "docker kill %; docker rm %"
+}
+
+function docker-image-most-recent() {
+    docker images | grep -v ^REPOSITORY | head -n1 | awk '{print $3}'
+}
+
+function docker-image-remove-all() {
+    docker-container-remove-all
+    docker images -a | grep -v ^REPOSITORY | awk '{print $3}' | xargs -r docker rmi
+}
+
+function docker-image-remove-orphan() {
+    docker images | grep "<none>" | awk '{print $3}' | xargs -r docker rmi
+}
+
